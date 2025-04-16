58887700d4901df358c75d018aa281c391b83f54
analauracano <laura.fioratti@outlook.com>
2025-04-16 14:24:42 -0300

    yarn e rotas

---
+++ src/containers/Movies/index.jsx
@@ -0,0 +1,10 @@
+function Movies() {
+  return (
+    <div>
+      <h1>Filmes</h1>
+      <p>Welcome to the filmes page!</p>
+    </div>
+  );
+}
+
+export default Movies;
---
+++ src/containers/Series/index.jsx
@@ -0,0 +1,10 @@
+function Series() {
+  return (
+    <div>
+      <h1>Series</h1>
+      <p>Welcome to the series page!</p>
+    </div>
+  );
+}
+
+export default Series;
